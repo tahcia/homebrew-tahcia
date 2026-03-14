6a304c4f2181affa3ae7231529a6e11470c925a3
Tahcia <it.is.tahcia@gmail.com>
2026-03-14 13:01:12 -0700

    Add Mac Keyboard+Mouse Control

--- Formula/tahcia.rb
+++ Formula/tahcia.rb
@@ -3,13 +3,14 @@ class Tahcia < Formula
   homepage "https://www.tahcia.com"
   version "1.0.0"
   url "https://github.com/tahcia/cli/releases/download/v1.0/tahcia.zip"
-  sha256 "72dd4e95512c5c8190919f32cf21829b6de68950d573eefb826cd6775be49939"
+  sha256 "57a7f04e212b013d822fcc6bf535734bea6f4602011713c7c1f74fda0fa59f11"
 
   depends_on "node"
 
   def install
-    bin.install "tahcia"
+    bin.install "tahcia"    
     bin.install "tahcia.js"
+    bin.install "tahcia-mac"
   end
 
   test do
