2770492c09bc75f7202002257fe735e3175b30c4
Tahcia <it.is.tahcia@gmail.com>
2026-05-15 15:27:31 -0700

    Update tahcia.rb

--- Formula/tahcia.rb
+++ Formula/tahcia.rb
@@ -2,7 +2,7 @@ class Tahcia < Formula
   desc "Tahcia CLI tunnel"
   homepage "https://www.tahcia.com"
   version "1.0.6"
-  url "https://github.com/tahcia/cli/releases/download/v1.0.4/tahcia.zip"
+  url "https://github.com/tahcia/cli/releases/download/v1.0.6/tahcia.zip"
   sha256 "d4e31ff2f9a2cfd8b74b45ceb2740730de2b6f0910733e3e1dd13759ca0f7bc7"
 
   depends_on "node"
