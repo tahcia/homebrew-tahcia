125b3215359a1fe9c1dd0543211a655a567bc244
Tahcia <it.is.tahcia@gmail.com>
2026-06-04 01:05:07 -0700

    v1.0.10
    Adding whatsapp

--- Formula/tahcia.rb
+++ Formula/tahcia.rb
@@ -1,9 +1,9 @@
 class Tahcia < Formula
   desc "Tahcia CLI tunnel"
   homepage "https://www.tahcia.com"
-  version "1.0.9"
-  url "https://github.com/tahcia/cli/releases/download/v1.0.9/tahcia.zip"
-  sha256 "07a4073e833c10c856415464363702c3b795604b1cec95322acd4b84a3010864"
+  version "1.0.10"
+  url "https://github.com/tahcia/cli/releases/download/v1.0.10/tahcia.zip"
+  sha256 "3fdd020dd0c7ea1e68bee9d85f67b903ce5ab9480b9e8215755c2e962e9ad51e"
 
   depends_on "node"
 
